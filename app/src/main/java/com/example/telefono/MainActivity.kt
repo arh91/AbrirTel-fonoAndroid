ef630fec0b78bc072e43976261cc8e5d5ba1e5f9
arh91 <alvaro_rh_991@hotmail.com>
2023-01-16 16:13:41 +0100

    aplicación que abre el teclado para hacer una llamada

---
+++ app/src/main/java/com/example/telefono/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.telefono
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // El botón para iniciar el teléfono
+        val callButton = findViewById<Button>(R.id.call)
+        // numero de telefono al que queremos llamar
+        val nro = findViewById<EditText>(R.id.nro)
+
+
+        callButton.setOnClickListener{
+            // creamos un intent específico que iniciará el teléfono
+            val intent = Intent(Intent.ACTION_DIAL)
+            // rellenamos el intent con el número
+            intent.data = Uri.parse("tel:" + nro.text)
+            // llamamos a la activity treléfono
+            startActivity(intent)
+        }
+    }
+}
